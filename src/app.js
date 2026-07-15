029527e3cc8d0a6aff0b0684038c96e0ec4aa1b3
Bhumika Sahai <sahaibhumika04@gmail.com>
2026-07-16 00:32:22 +0530

    commit

--- src/app.js
+++ src/app.js
@@ -4,3 +4,45 @@ const express = require("express")
 
 const app = express()
 
+app.use(express.json())
+
+
+/*
+
+note = {
+    title : "my first note",
+    description : "this is my first note"
+}
+
+
+const notes = {
+    {
+        title : "my first note",
+        description : "this is my first note"
+    },
+    {
+        title : "my first note",
+        description : "this is my first note"
+    },
+    {
+        title : "my first note",
+        description : "this is my first note"
+    },
+}
+
+*/
+
+const notes = []
+
+/* title, description */
+
+app.post('/notes', (req,res)=>{
+    notes.push(req.body)
+
+
+    res.status(201).json({
+        message : "note created successfully"
+    })
+})
+
+module.exports = app
