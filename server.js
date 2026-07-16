fdac72f82b235fa5822351f07633f76196f49148
Bhumika Sahai <sahaibhumika04@gmail.com>
2026-07-17 00:40:25 +0530

    update

--- server.js
+++ server.js
@@ -2,6 +2,6 @@
 
 const app = require("./src/app")
 
-app.listen(3000, () => {
-    console.log("server is running on port 3000");
+app.listen(5000, () => {
+    console.log("server is running on port 5000");
 })
--- src/app.js
+++ src/app.js
@@ -35,13 +35,40 @@ const notes = {
 const notes = []
 
 /* title, description */
-
+/* POST - /notes */
 app.post('/notes', (req,res)=>{
     notes.push(req.body)
 
-
     res.status(201).json({
         message : "note created successfully"
+    })
+})
+
+/* GET - /notes */
+app.get('/notes', (req,res) => {
+    res.status(200).json({
+        message : "notes fetched successfully",
+        notes : notes
+    })
+})
+
+/* DELETE - /notes/1(index) 1 is the index from notes[] array */
+app.delete('/notes/:index',(req,res)=>{
+    console.log("deleted api")
+    const index = req.params.index  //index = 1 as per the comment above
+    delete notes[ index ]
+    res.status(200).json({
+        message : "note deleted successfully"
+    })      
+})
+
+/* PATCH - /notes/index */
+app.patch('/notes/:index',(req,res)=>{
+    const index = req.params.index
+    const description = req.body.description
+    notes[ index ].description = description
+    res.status(200).json({
+        message : "note updated succesfully"
     })
 })
 
